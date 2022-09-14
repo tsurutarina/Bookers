f7208f60c15a49f0f010291e2915618181d28a8a
EC2 Default User <ec2-user@ip-172-31-9-131.ap-northeast-1.compute.internal>
2022-09-14 06:56:01 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully creted."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :index
     end
